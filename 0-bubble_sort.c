67188cebc662fbec26d4e4fcb96d9259bda2461b
Chidi <chidiamaraekejiuba@gmail.com>
2023-02-21 17:35:59 -0800

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap_integers - function that swaps two integers
+ * @a: First integer
+ * @b: Second integer
+ */
+void swap_integers(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: array of integers
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int swapped = 1;
+
+	if (!array || size < 2)
+		return;
+	while (swapped != 0)
+	{
+		swapped = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_integers(array + j, array + (j + 1));
+				print_array((const int *)array, size);
+				swapped = 1;
+			}
+		}
+		i++;
+		if (swapped == 0)
+			break;
+	}
+}
